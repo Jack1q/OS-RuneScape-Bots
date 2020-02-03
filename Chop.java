95203d74d53b9d288e79bea9224c3510f050abc9
Jack Donofrio <53123020+Jack1q@users.noreply.github.com>
2020-02-02 21:55:18 -0500

    Added area to chop.
    Added treeArea. Only chop if player is in treeArea. If not in treeArea, walk to treeArea.

--- Chop.java
+++ Chop.java
@@ -1,30 +1,38 @@
+import org.dreambot.api.methods.map.Area;
+import org.dreambot.api.methods.map.Map;
 import org.dreambot.api.script.AbstractScript;
+import org.dreambot.api.script.Category;
 import org.dreambot.api.script.ScriptManifest;
 import org.dreambot.api.wrappers.interactive.GameObject;
-import org.dreambot.api.script.Category;
 
 @ScriptManifest(author = "Jack Donofrio", name = "Jack's Tree Cutter", version = 1.0, description = "Simple Tree Cutter and Log Burner", category = Category.WOODCUTTING)
 public class Chop extends AbstractScript
 {
+  Area treeArea = new Area(3071, 3314, 3122, 3299);
 
   @Override
   public int onLoop()
   {
-    if (getInventory().isFull())
+    if (treeArea.contains(getLocalPlayer()))
     {
-      getInventory().get("Tinderbox").useOn("Logs");
-      sleep(3000);
-    }
-    if (!getLocalPlayer().isAnimating())
-    {
-      GameObject tree = getGameObjects().closest("Tree");
-      if (tree != null)
+      if (getInventory().isFull())
       {
-        tree.interact("Chop down");
+        getInventory().get("Tinderbox").useOn("Logs");
+        sleep(3000);
+      }
+      if (!getLocalPlayer().isAnimating())
+      {
+        GameObject tree = getGameObjects().closest("Tree");
+        if (tree != null)
+        {
+          tree.interact("Chop down");
+        }
       }
     }
+    else
+    {
+      getWalking().walk(treeArea.getRandomTile());
+    }
     return 1000;
-
   }
-
 }
