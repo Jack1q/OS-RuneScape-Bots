56a02895bdcb911dd20a7c56cc546dcfde807b29
Jack Donofrio <53123020+Jack1q@users.noreply.github.com>
2020-02-02 20:30:09 -0500

    Update Chop.java

--- Chop.java
+++ Chop.java
@@ -1,8 +1,6 @@
-import org.dreambot.api.methods.Calculations;
 import org.dreambot.api.script.AbstractScript;
 import org.dreambot.api.script.ScriptManifest;
 import org.dreambot.api.wrappers.interactive.GameObject;
-import org.dreambot.api.wrappers.items.GroundItem;
 import org.dreambot.api.script.Category;
 
 @ScriptManifest(author = "Jack Donofrio", name = "Jack's Tree Cutter", version = 1.0, description = "Simple Tree Cutter and Log Burner", category = Category.WOODCUTTING)
